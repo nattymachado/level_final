6f6920a0bdc2a6432d79e1fdb2127d26d5515128
Natalia Machado dos Santos <nati.cardoso.machado@gmail.com>
2019-02-22 18:07:24 -0300

    Including camera moviment

---
+++ source/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private Vector3 point;//the coord to the point where the camera looks at
+    [SerializeField] private float speedMod = 10.0f;//a speed modifier
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        point = target.position;//get target's coords
+        transform.LookAt(point);//makes the camera look to it
+
+    }
+
+    public void RotateCameraToRight()
+    {
+        if (target)
+        {
+            transform.RotateAround(point, new Vector3(0.0f, -1.0f, 0.0f), Time.deltaTime * speedMod);
+        }
+    }
+
+    public void RotateCameraToLeft()
+    {
+        if (target)
+        {
+            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), Time.deltaTime * speedMod);
+        }
+    }
+
+
+
+}
---
+++ source/Assets/Scripts/GameUIBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIBehaviour : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera = null;
+
+
+
+    public void RightButtonOnClick()
+    {
+        CameraBehaviour cameraBehaviour = mainCamera.GetComponent<CameraBehaviour>();
+        cameraBehaviour.RotateCameraToRight();
+    }
+}
