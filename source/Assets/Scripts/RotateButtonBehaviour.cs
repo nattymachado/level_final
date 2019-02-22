946ba44bc7fd92da8bb047caac7dc4da5076a346
Natalia Machado dos Santos <nati.cardoso.machado@gmail.com>
2019-02-22 18:07:24 -0300

    Including camera moviment

---
+++ source/Assets/Scripts/RotateButtonBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum Direction
+{
+    Right,
+    Left
+}
+public class RotateButtonBehaviour : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Direction direction = Direction.Right;
+    bool pressed = false;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+    void Update()
+    {
+        if (!pressed)
+            return;
+        CameraBehaviour cameraBehaviour = mainCamera.GetComponent<CameraBehaviour>();
+        if (direction == Direction.Right)
+        {
+            cameraBehaviour.RotateCameraToRight();
+        } else
+        {
+            cameraBehaviour.RotateCameraToLeft();
+        }
+        
+    }
+}
