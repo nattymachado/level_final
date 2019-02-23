e44b3ca354c2aeadc7b56eb1b37d1cd910e5acb6
Natalia Machado dos Santos <nati.cardoso.machado@gmail.com>
2019-02-23 19:14:27 -0300

    Improvements

--- source/Assets/Scripts/CameraBehaviour.cs
+++ source/Assets/Scripts/CameraBehaviour.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    private Vector3 point;//the coord to the point where the camera looks at
     [SerializeField] private float speedMod = 10.0f;//a speed modifier
+    [SerializeField] private float upLimit = 10.0f;//limit to up moviment
+    [SerializeField] private float downLimit = -10.0f;//limit to up moviment
+    private Vector3 point;//the coord to the point where the camera looks at
+    
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +36,35 @@ public class CameraBehaviour : MonoBehaviour
         }
     }
 
+    public void RotateCameraToUp()
+    {
+        float rotationOnX = transform.eulerAngles.x;
+        if (transform.eulerAngles.x > 180)
+        {
+            rotationOnX = transform.eulerAngles.x - 360;
+        }
 
+        Debug.Log(rotationOnX);
+        if ((target) && (rotationOnX < upLimit))
+        {
+            transform.RotateAround(point, new Vector3(1.0f, 0.0f, 0.0f), Time.deltaTime * speedMod);
+            
+        }
+    }
+
+    public void RotateCameraToDown()
+    {
+        float rotationOnX = transform.eulerAngles.x;
+        if (transform.eulerAngles.x > 180)
+        {
+            rotationOnX = transform.eulerAngles.x - 360;
+        }
+        
+        Debug.Log(rotationOnX);
+        if ((target) && (rotationOnX > downLimit))
+        {
+            transform.RotateAround(point, new Vector3(-1.0f, 0.0f, 0.0f), Time.deltaTime * speedMod);
+        }
+    }
 
 }
