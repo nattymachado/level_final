55b371771af65b59455d1c830479a07b3e4f39a5
Natalia Machado dos Santos <nati.cardoso.machado@gmail.com>
2019-02-27 18:06:57 -0300

    Including zoom

--- source/Assets/Scripts/TouchManager.cs
+++ source/Assets/Scripts/TouchManager.cs
@@ -12,6 +12,7 @@ public class TouchManager : MonoBehaviour
 
     private Vector2 startPos;
     private CameraBehaviour cameraBehaviour;
+    private float perspectiveZoomSpeed = 0.5f;
 
     private void Start()
     {
@@ -20,7 +21,29 @@ public class TouchManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // Otherwise change the field of view based on the change in distance between the touches.
+            mainCamera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+            // Clamp the field of view to make sure it's between 0 and 180.
+            mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 5f, 24f);
+        } else if (Input.touchCount == 1)
         {
             Touch touch = Input.touches[0];
             switch (touch.phase)
