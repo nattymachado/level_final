e05132b89f22e7bebd80c5d7f6c373391886e9f6
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2019-03-24 23:09:30 -0300

    Sample levels and level selector

---
+++ source/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+  public void LoadScene(string sceneName)
+  {
+    if (Application.CanStreamedLevelBeLoaded(sceneName))
+    {
+      SceneManager.LoadScene(sceneName);
+    }
+    else
+    {
+      Debug.LogError("Scene " + sceneName + " does not exists.");
+    }
+  }
+
+  public void LoadLevelSelector(){
+      LoadScene("LevelSelection");
+  }
+}
