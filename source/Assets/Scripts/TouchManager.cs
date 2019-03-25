bdc9c70801b5d18647ec7d35efbd1b07852ab8ca
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2019-03-25 00:16:54 -0300

    More test levels + adjustments touch

--- source/Assets/Scripts/TouchManager.cs
+++ source/Assets/Scripts/TouchManager.cs
@@ -5,91 +5,95 @@ using UnityEngine.EventSystems;
 
 public class TouchManager : MonoBehaviour
 {
-    public float MinSwipeDistX;
+  public float MinSwipeDistX;
 
-    [SerializeField] private Camera mainCamera;
-    [SerializeField] private CharacterBehaviour character;
-    [SerializeField] private float speed = 10;
-    [SerializeField] private GameObject zoomPointer;
+  [SerializeField] private Camera mainCamera;
+  [SerializeField] private CharacterBehaviour character;
+  [SerializeField] private float speed = 10;
+  [SerializeField] private GameObject zoomPointer;
 
-    private Vector2 startPos;
-    private CameraBehaviour cameraBehaviour;
-    private float perspectiveZoomSpeed = 0.2f;
+  private Vector2 startPos;
+  private CameraBehaviour cameraBehaviour;
+  private float perspectiveZoomSpeed = 0.2f;
+  private bool isMoving = false;
 
-    private void Start()
+  private void Start()
+  {
+    cameraBehaviour = mainCamera.GetComponent<CameraBehaviour>();
+  }
+
+  void Update()
+  {
+    if (Input.touchCount == 2)
     {
-        cameraBehaviour = mainCamera.GetComponent<CameraBehaviour>();
-    }
+      // Store both touches.
+      Touch touchZero = Input.GetTouch(0);
+      Touch touchOne = Input.GetTouch(1);
 
-    void Update()
+
+      zoomPointer.transform.position = (new Vector3(touchZero.position.x, touchZero.position.y, 0f) + new Vector3(touchOne.position.x, touchOne.position.y, 0f)) / 2;
+
+      // Find the position in the previous frame of each touch.
+      Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+      Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+      // Find the magnitude of the vector (the distance) between the touches in each frame.
+      float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+      float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+      // Find the difference in the distances between each frame.
+      float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+      // Otherwise change the field of view based on the change in distance between the touches.
+      mainCamera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+      // Clamp the field of view to make sure it's between 0 and 180.
+      mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 5f, 24f);
+    }
+    else if (Input.touchCount == 1)
     {
-        if (Input.touchCount == 2)
-        {
-            // Store both touches.
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
+      Touch touch = Input.touches[0];
 
+      switch (touch.phase)
+      {
+        case TouchPhase.Began:
+          startPos = touch.position;
+          isMoving = false;
+          break;
+        case TouchPhase.Moved:
+          isMoving = RotateCamera(touch) || isMoving;
+          startPos = touch.position;
+          break;
+        case TouchPhase.Ended:
+          if (!isMoving)
+          {
+            character.Move(touch.position);
+          }
+          isMoving = false;
+          break;
+      }
 
-            zoomPointer.transform.position = (new Vector3(touchZero.position.x, touchZero.position.y, 0f) + new Vector3(touchOne.position.x, touchOne.position.y, 0f)) / 2;
-
-            // Find the position in the previous frame of each touch.
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            // Find the magnitude of the vector (the distance) between the touches in each frame.
-            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-            // Find the difference in the distances between each frame.
-            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-            // Otherwise change the field of view based on the change in distance between the touches.
-            mainCamera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-
-            // Clamp the field of view to make sure it's between 0 and 180.
-            mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, 5f, 24f);
-        } else if (Input.touchCount == 1)
-        {
-            Touch touch = Input.touches[0];
-            bool isMoving = false;
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    startPos = touch.position;
-                    break;
-                case TouchPhase.Moved:
-                    isMoving = RotateCamera(touch);
-                    startPos = touch.position;
-                    break;
-                case TouchPhase.Ended:
-                    if (!isMoving)
-                    {
-                        character.Move(touch.position);
-                    }
-                    break;
-            }
-
-        }
     }
+  }
 
-    private bool RotateCamera(Touch touch)
+  private bool RotateCamera(Touch touch)
+  {
+    float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+    if (swipeDistHorizontal > MinSwipeDistX)
     {
-        float swipeDistHorizontal = (new Vector3(touch.position.x, 0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
-        if (swipeDistHorizontal > MinSwipeDistX)
-        {
 
-            float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+      float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
 
-            if (swipeValue > 0)
-            {
-                cameraBehaviour.RotateCameraToLeft(Mathf.Abs(swipeValue) * speed);
-            }
-            else if (swipeValue < 0)
-            {
-                cameraBehaviour.RotateCameraToRight(Mathf.Abs(swipeValue) * speed);
-            }
-            return true;
-        }
-        return false;
+      if (swipeValue > 0)
+      {
+        cameraBehaviour.RotateCameraToLeft(Mathf.Abs(swipeValue) * speed);
+      }
+      else if (swipeValue < 0)
+      {
+        cameraBehaviour.RotateCameraToRight(Mathf.Abs(swipeValue) * speed);
+      }
+      return true;
     }
+    return false;
+  }
 }
