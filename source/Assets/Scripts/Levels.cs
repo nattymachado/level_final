477b0dd43a45b40ddea26c215f7caea572d9e61b
Thiago guardado Beltran <thiagoguardado@gmail.com>
2019-04-25 21:13:15 -0300

    Classes de save e level

---
+++ source/Assets/Scripts/Levels.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LevelName {NULL,Robo,Seguranca,Cachorro}
+
+[CreateAssetMenu(fileName = "Levels", menuName = "Levels", order = 1)]
+public class Levels : ScriptableObject
+{
+    public List<LevelDefs> levels;
+}
---
+++ source/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveManager
+{
+    private static PlayerProgress currentProgress;
+    private static string filePath = Application.persistentDataPath + "/gamesave.save";
+
+    public static PlayerProgress LoadProgressFile(){
+        PlayerProgress save = null;
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(filePath, FileMode.Open);
+            save = (PlayerProgress)bf.Deserialize(file);
+            file.Close();
+            Debug.Log("Progress Loaded");
+        }
+        return save;
+    }
+
+    public static void SaveProgressFile(PlayerProgress save){
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+        Debug.Log("Progress Saved");
+    }
+
+    public static void AddLevel(LevelName levelName, bool concluded){
+        currentProgress.levels.Add(new LevelProgress(levelName,concluded));
+    }
+}
