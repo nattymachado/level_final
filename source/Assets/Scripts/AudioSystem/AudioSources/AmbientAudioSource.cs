0ff400af84713ad044d729f7b1790a00d91ae513
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-05-01 19:27:28 -0300

    Progress on Audio

---
+++ source/Assets/Scripts/AudioSystem/AudioSources/AmbientAudioSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientAudioSource : BaseSFXAudioController
+{
+    //Reference Variables
+    [Header("Audio Clip Reference")]
+    [SerializeField] protected AudioClip _audioClip;
+
+    //Control Variables
+    [Header("Control Variables")]
+    [SerializeField] protected bool _playOnAwake;
+
+    //Start
+    protected override void Start()
+    {
+        base.Start();
+        _audioSource.clip = _audioClip;
+        _audioSource.playOnAwake = _playOnAwake;
+        _audioSource.loop = true;
+        if(_playOnAwake) _audioSource.Play();
+    }
+}
