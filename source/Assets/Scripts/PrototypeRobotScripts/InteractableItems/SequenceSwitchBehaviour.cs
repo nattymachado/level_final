35a32c0b066d72761884b31a58895ef0b4b76d77
Natalia Machado <natalia.machado@hp.com>
2019-05-01 04:59:16 -0300

    Including moviments on crystal

---
+++ source/Assets/Scripts/PrototypeRobotScripts/InteractableItems/SequenceSwitchBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace prototypeRobot
+{
+    public class SequenceSwitchBehaviour : InteractableItemBehaviour
+    {
+        [SerializeField] public int id;
+        private bool _isOn;
+
+
+        protected override void ExecuteAction(Collider other)
+        {
+            if (!_isOn)
+            {
+                transform.Rotate(0, 0, -180);
+                GetComponentInParent<SwitchControllerBehaviour>().CheckSwitchPosition(id);
+                SetActive(false);
+                _isOn = true;
+            }
+            
+        }
+
+        public void Clear()
+        {
+            if (_isOn)
+            {
+                SetActive(false);
+                transform.Rotate(0, 0, -180);
+                _isOn = false;
+            }
+            
+        }
+
+    }
+}
---
+++ source/Assets/Scripts/PrototypeRobotScripts/InteractableItems/TeletransportBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+namespace prototypeRobot
+{
+    public class TeletransportBehaviour : InteractableItemBehaviour
+    {
+        [SerializeField] Transform endPosition;
+        private CharacterBehaviour _character;
+        private bool _canMove = false;
+
+        protected override void ExecuteAction(Collider other)
+        {
+            _character = other.GetComponent<CharacterBehaviour>();
+            if (_character && !_canMove)
+            {
+                
+                _canMove = true;
+            }
+            
+        }
+
+        private void Update()
+        {
+            if (_canMove && _character != null)
+            {
+                _character.GetComponent<NavMeshAgent>().enabled = false;
+                _character.transform.position = endPosition.position;
+                _character.GetComponent<NavMeshAgent>().enabled = true;
+                _character = null;
+            }
+        }
+
+    }
+}
