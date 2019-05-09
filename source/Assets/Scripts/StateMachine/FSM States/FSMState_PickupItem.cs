273d36b8d14d302b36aa7ba7b6ffe9a49bc9a946
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2019-05-09 18:22:30 -0300

    Base FSM

---
+++ source/Assets/Scripts/StateMachine/FSM States/FSMState_PickupItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMState_PickupItem : FSMState
+{
+    //Constructor
+    public FSMState_PickupItem(FSMController FSMControllerRef) : base(FSMControllerRef, GameEnums.FSMInteractionEnum.PickupItem) { }
+
+    public override void OnStateEnter()
+    {
+        FSMControllerRef.lockedByInteraction = true;
+    }
+
+    public override void OnStateExit()
+    {
+        FSMControllerRef.SetNextState(GameEnums.FSMInteractionEnum.Idle);
+    }
+
+    public override void OnStateUpdate() { }
+}
