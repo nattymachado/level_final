e64ed632ff106468a6e804f71bab079224aeec18
Natalia Machado <natalia.machado@hp.com>
2019-05-14 20:29:26 -0300

    removing invalid logs

--- source/Assets/Scripts/InteractableItems/InitRampBehaviour.cs
+++ source/Assets/Scripts/InteractableItems/InitRampBehaviour.cs
@@ -9,7 +9,6 @@ public class InitRampBehaviour : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Entrei ...");
         CharacterBehaviour character = other.GetComponent<CharacterBehaviour>();
 
         if (!isActive)
@@ -19,7 +18,6 @@ public class InitRampBehaviour : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("SAi ...");
         if (!isActive && end.isActive)
         {
             isActive = true;
