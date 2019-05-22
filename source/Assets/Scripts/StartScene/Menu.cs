aa4ff69bb3589dcf44b2792c03d3f5cc7732032d
Natalia Machado <natalia.machado@hp.com>
2019-05-22 13:52:45 -0300

    Init configuration menu

---
+++ source/Assets/Scripts/StartScene/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace StartMenu
+{
+    public class Menu : MonoBehaviour
+    {
+        [SerializeField] private string _nextScene;
+        [SerializeField] private GameObject _configurationPanel;
+        [SerializeField] private GameObject _principalPanel;
+
+        public void Play()
+        {
+            SceneManager.LoadScene(_nextScene, LoadSceneMode.Single);
+        }
+
+        public void Configuration()
+        {
+            _configurationPanel.SetActive(true);
+            _principalPanel.SetActive(false);
+        }
+
+        public void Close()
+        {
+            _configurationPanel.SetActive(false);
+            _principalPanel.SetActive(true);
+        }
+    }
+}
