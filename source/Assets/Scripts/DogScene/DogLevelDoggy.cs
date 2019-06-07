94f43334c4e47ec7c1cf6b0fe6a979a47ee82c05
Thiago guardado Beltran <thiagoguardado@gmail.com>
2019-06-06 22:27:21 -0300

    animações level cachorro

---
+++ source/Assets/Scripts/DogScene/DogLevelDoggy.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogLevelDoggy : MonoBehaviour
+{
+    public void GoToBowl(){
+        Debug.Log("dog start go to bowl");
+    }
+}
