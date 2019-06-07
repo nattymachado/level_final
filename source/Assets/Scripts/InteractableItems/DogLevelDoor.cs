5125ff85e398e2b5510aeecb4b02d6ef82728330
Thiago guardado Beltran <thiagoguardado@gmail.com>
2019-06-06 21:08:46 -0300

    animações portas fase cachorro

---
+++ source/Assets/Scripts/InteractableItems/DogLevelDoor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogLevelDoor : MonoBehaviour
+{
+  private bool opened = false;
+  [SerializeField] private Animator animator;
+
+  private void Open(){
+    animator.SetBool("opened",true);
+    opened = true;
+    // Debug.Log("open");
+  }
+
+  private void Close(){
+    animator.SetBool("opened",false);
+    opened = false;
+    // Debug.Log("close");
+  }
+
+  public void Toggle(){
+    if(opened) Close(); else Open();
+  }
+
+}
