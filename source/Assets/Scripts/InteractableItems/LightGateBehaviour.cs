a7f98bba4a9d188d5d026343b1fa0b3c0735e7f2
Natalia Machado <natalia.machado@hp.com>
2019-12-27 17:41:40 -0300

    Including lights

---
+++ source/Assets/Scripts/InteractableItems/LightGateBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class LightGateBehaviour : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _lights;
+    Animator _gateAnimator;
+
+    private void Start()
+    {
+        _gateAnimator = GetComponent<Animator>();
+    }
+
+    public IEnumerator OpenGate()
+    {
+        if (!(_lights.Any(i => i.activeSelf == false))) {
+            yield return new WaitForSeconds(0.1f);
+            _gateAnimator.SetBool("Open", true);
+        }
+       
+    }
+
+
+}
---
+++ source/Assets/Scripts/InteractableItems/SpotlightBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SpotlightBehaviour : InteractableItemBehaviour
+{
+
+    [SerializeField] string lightName;
+    [SerializeField] List<GameObject> _lights;
+    [SerializeField] List<LightGateBehaviour> _gates;
+
+    protected override void ExecuteAction(CharacterBehaviour character)
+    {
+        if (character && character.CheckInventaryObjectOnSelectedPosition(lightName))
+        {
+            
+
+            for (int i = 0; i < _lights.Count; i++)
+            {
+                _lights[i].SetActive(true);
+            }
+
+            for (int i = 0; i < _gates.Count; i++)
+            {
+                Debug.Log("Executing 2");
+                StartCoroutine(_gates[i].OpenGate());
+            }
+        }
+    }
+
+   
+
+}
